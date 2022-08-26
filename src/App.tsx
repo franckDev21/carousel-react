8f3b77257f015b43e3a8e6f808d88107cc384445
franckDev21 <tutodevphp@gmail.com>
2022-08-26 14:38:27 +0100

    add carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Image1 from './assets/image_1.jpg'
+import Image2 from './assets/image_2.jpg'
+import Image3 from './assets/image_3.jpg'
+import Image4 from './assets/image_4.jpg'
 import './App.scss'
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Carousel interval={5000} 
+        showStatus={false} 
+        showIndicators={false} 
+        showThumbs={false} 
+        infiniteLoop 
+        autoPlay 
+        className='carousel'>
+
+        <div className='carousel__img'>
+          <img src={Image1} alt="" />
+        </div>
+        <div className='carousel__img'>
+          <img src={Image2} alt="" />
+        </div>
+        <div className='carousel__img'>
+          <img src={Image3} alt="" />
+        </div>
+        <div className='carousel__img'>
+          <img src={Image4} alt="" />
+        </div>
+
+      </Carousel>
     </div>
   )
 }
